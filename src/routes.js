cc5b758ac6b5d7daf2b6dfffbfe44be1132e7cbd
fanfan <280493732@qq.com>
2019-05-17 08:49:39 +0800

    品牌管理（查和删），类型管理树形菜单

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,16 @@
 import Login from './views/Login.vue'
 import NotFound from './views/404.vue'
 import Home from './views/Home.vue'
-import Main from './views/Main.vue'
-import Table from './views/nav1/Table.vue'
 import Form from './views/nav1/Form.vue'
 import user from './views/nav1/user.vue'
 import Page4 from './views/nav2/Page4.vue'
 import Page5 from './views/nav2/Page5.vue'
 import Page6 from './views/nav3/Page6.vue'
 import echarts from './views/charts/echarts.vue'
+import brand from './views/product/Brand.vue'
+import productType from './views/product/ProductType.vue'
+
+
 
 let routes = [
     {
@@ -39,8 +41,8 @@ let routes = [
         name: '商品模块',
         iconCls: 'el-icon-message',//图标样式class
         children: [
-            { path: '/main', component: Main, name: '商品品牌', hidden: true },
-            { path: '/table', component: Table, name: '商品类型' },
+            { path: '/brand', component: brand, name: '商品品牌'},
+            { path: '/productType', component: productType, name: '商品类型' },
             { path: '/form', component: Form, name: '商品模板' },
             { path: '/user', component: user, name: '商品规格' },
             { path: '/page4', component: Page4, name: '商品管理' },
